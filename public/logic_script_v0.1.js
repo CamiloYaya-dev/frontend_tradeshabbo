0f31cc1fcd1171cd7c94c7618aa7b33fb85bda27
Camilo Yaya - Emo. <saenz292761@gmail.com>
2025-01-08 22:56:08 -0500

    rename description history

--- public/logic_script_v0.1.js
+++ public/logic_script_v0.1.js
@@ -806,7 +806,7 @@ function initialize() {
                                     </tbody>
                                 </table>
                             </div>
-                            <p class="price_history_content habbo_text_blue mobile_description">${firstRecord.descripcion == '' ? "Este furni aun no ha salido a la venta" : firstRecord.descripcion}</p>
+                            <p class="price_history_content habbo_text_blue mobile_description">${firstRecord.descripcion == '' ? "No ahi descripcion disponible" : firstRecord.descripcion}</p>
                         `;
                         $('#product-history-container').html(historyContent);
                         selectedLanguage
