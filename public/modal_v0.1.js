c986cc96cc3dedd34c1987c573d6bec45bb0c8aa
Camilo Yaya - Emo. <saenz292761@gmail.com>
2025-03-15 18:46:51 -0500

    Add modal embebed twitter

--- public/modal_v0.1.js
+++ public/modal_v0.1.js
@@ -1,24 +1,36 @@
-/*$('body').append(`
-    <div class="modal" id="priceGuidelineModal" tabindex="-1" role="dialog" aria-labelledby="priceGuidelineModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal_olimpiadas" role="document">
-            <div class="modal-content online_users_content">
-                <div class="modal-header">
-                    <h5 class="modal-title habbo_text_blue" id="priceGuidelineModalLabel">Aviso Importante</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body text-center">
-                    <p class="online_habbo_text_white_fz_15">
-                       El precio del vip bajo, paso de 60c a 50c por lo que el precio de los rares, funkys y mega rares "EN TERMINOS DE CREDITOS" bajo, sin embargo en vips siguen al mismo precio, es por esto que puede mostrar que los diferentes furnis bajaron de precio PERO siguen valiendo lo mismo en vips.
-                    </p>
-                    <img id="noticiaModalImage" src="./furnis/hc/El_Club_Sofa.png" alt="Bajo el precio del vip" class="img-fluid mb-3 olimpiadas_imagen">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
+$(document).ready(function () {
+    if (!localStorage.getItem('priceGuidelineModalSeen')) {
+        $('body').append(`
+            <div class="modal" id="priceGuidelineModal" tabindex="-1" role="dialog" aria-labelledby="priceGuidelineModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal_olimpiadas" role="document">
+                    <div class="modal-content online_users_content">
+                        <div class="modal-header">
+                            <h5 class="modal-title habbo_text_blue" id="priceGuidelineModalLabel">Un Nuevo evento se aproxima</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body text-center">
+                            <blockquote class="twitter-tweet">
+                                <p lang="es" dir="ltr">
+                                    Atención Comunidad de .ES<br><br>🚀 ¡Se viene Kingdom’s Lab! 🏆✨<br>El evento con la mayor premiación en juego 🎉<br><br>📅 22 de marzo<br>⏰ 17H<br><br>🔎 ¿Estás preparado? Pronto más información… 🔥<br><br>Nuestra web: <a href="https://t.co/7VAzVTKrGY">https://t.co/7VAzVTKrGY</a><br>Nuestro servidor de Discord: <a href="https://t.co/CL2D9aZrZF">https://t.co/CL2D9aZrZF</a>… <a href="https://t.co/4uht81WXPe">pic.twitter.com/4uht81WXPe</a>
+                                </p>&mdash; Origins Kingdom (@OriginsKingdom) <a href="https://twitter.com/OriginsKingdom/status/1901048068658905359?ref_src=twsrc%5Etfw">March 15, 2025</a>
+                            </blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-`);
-$('#priceGuidelineModal').modal('show');*/
+        `);
+
+        // Mostrar el modal
+        $('#priceGuidelineModal').modal('show');
+
+        // Guardar en localStorage cuando el modal se cierre
+        $('#priceGuidelineModal').on('hidden.bs.modal', function () {
+            localStorage.setItem('priceGuidelineModalSeen', 'true');
+        });
+    }
+});
